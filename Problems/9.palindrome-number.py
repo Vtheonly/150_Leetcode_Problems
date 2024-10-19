c1ec655babdb70403ba2fc82f9cc6bc432d60f54
mersel fares <merselfaresw@gmail.com>
2024-10-19 10:32:46 +0100

    first problem

---
+++ Problems/9.palindrome-number.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=9 lang=python
+#
+# [9] Palindrome Number
+#
+# https://leetcode.com/problems/palindrome-number/description/
+#
+# algorithms
+# Easy (57.74%)
+# Likes:    13059
+# Dislikes: 2766
+# Total Accepted:    5.4M
+# Total Submissions: 9.3M
+# Testcase Example:  '121'
+#
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it
+# becomes 121-. Therefore it is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a
+# palindrome.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# -2^31 <= x <= 2^31 - 1
+# 
+# 
+# 
+# Follow up: Could you solve it without converting the integer to a string?
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        
+        return True
+# @lc code=end
+
